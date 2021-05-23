6befe7e95a71c66a118619ee922741e5b3dab7e3
Adedeji Rotibi <dayjeerow7@gmail.com>
2021-05-23 01:20:07 +0100

    remove prefiz

--- fix/cron.php
+++ fix/cron.php
@@ -20,7 +20,7 @@ function resetErrorTasks()
 {
     global $DB;
 
-    $reset = $DB->execute("UPDATE {mdl_tool_objectfs_objects} SET location=0 WHERE location=-1");
+    $reset = $DB->execute("UPDATE {tool_objectfs_objects} too SET location=0 WHERE location=-1");
 
     return $reset;
 }
@@ -29,9 +29,9 @@ function clearLock()
 {
     global $DB;
 
-    $releaseAdhoc = $DB->delete_records("mdl_lock_db", array("resourcekey" => '%adhoc%task%%'));
-    $releaseScheduleRunner = $DB->delete_records("mdl_lock_db", array("resourcekey" => '%schedule%runner%'));
-    $releaseResources = $DB->execute("UPDATE {mdl_lock_db} SET owner=NULL where expires IS NOT NULL AND resourcekey like '%objectfs%'");
+    $releaseAdhoc = $DB->delete_records("lock_db", array("resourcekey" => '%adhoc%task%%'));
+    $releaseScheduleRunner = $DB->delete_records("lock_db", array("resourcekey" => '%schedule%runner%'));
+    $releaseResources = $DB->execute("UPDATE {lock_db} SET owner=NULL where expires IS NOT NULL AND resourcekey like '%objectfs%'");
 
     // $clear = $DB->get_record_sql("DELETE FROM {mdl_lock_db} WHERE resourcekey LIKE '%adhoc%task%%';DELETE FROM {mdl_lock_db} WHERE resourcekey LIKE '%schedule%runner%';UPDATE  {mdl_lock_db} SET owner=NULL where expires IS NOT NULL AND resourcekey like '%objectfs%';");
 
