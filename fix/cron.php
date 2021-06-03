3f89518abce1a514e209d2323f43104e296c5bdc
Adedeji Rotibi <dayjeerow7@gmail.com>
2021-06-03 12:42:28 +0100

    is it working

--- fix/cron.php
+++ fix/cron.php
@@ -6,11 +6,12 @@ require(__DIR__ . '/../../../../config.php');
 
 defined('MOODLE_INTERNAL') || die;
 
+print_r("Error files before : " . fetchErrorFiles() . "\n");
 resetErrorTasks();
 clearLock();
 
 for ($i = 0; $i < 10; $i++) {
-    print_r("Running MAGIC");
+    print_r("Running MAGIC\n");
     execCron('php /var/www/html/admin/cli/scheduled_task.php --execute="tool_objectfs\task\push_objects_to_storage"');
     clearLock();
     execCron('php /var/www/html/admin/cli/scheduled_task.php --execute="tool_objectfs\task\delete_local_objects"');
@@ -18,12 +19,13 @@ for ($i = 0; $i < 10; $i++) {
 }
 
 echo "DONE\n";
+print_r("Error files after: " . fetchErrorFiles() . "\n");
 
 function resetErrorTasks()
 {
     global $DB;
 
-    print_r("Resetting error files");
+    print_r("Resetting error files\n");
     $reset = $DB->execute("UPDATE {tool_objectfs_objects} too SET location=0 WHERE location=-1");
 
     return $reset;
@@ -44,7 +46,7 @@ function fetchErrorFiles()
 {
     global $DB;
 
-    print_r("Fetching error files");
+    print_r("Fetching error files\n");
 
     $count = $DB->count_records("tool_objectfs_objects", array("location" => -1));
 
@@ -53,6 +55,6 @@ function fetchErrorFiles()
 
 function execCron($cron)
 {
-    print_r("Executing cron $cron");
+    print_r("Executing cron $cron\n");
     return shell_exec($cron);
 }
